19765929ddf6f1c736d229f2eecdac7642aa9c75
Ferg <ferg@Fergals-MacBook-Pro.local>
2025-12-16 22:40:19 +0000

    Added addition styles to the challenages page

--- src/pages/Challenages/Components/ChallenageItem.jsx
+++ src/pages/Challenages/Components/ChallenageItem.jsx
@@ -1,14 +1,45 @@
-
-
+import { BsCheck2Circle } from "react-icons/bs";
+import { FaRegHourglass } from "react-icons/fa";
 
 export default function ChallenageItem ({item}) {
+    const iconSelection = (status) => {
+        if (status === "Attempted") {
+            return (
+                <div className="flex items-center justify-center">
+                    <FaRegHourglass className="size-5" color="orange" />
 
+                </div>
+            )
+        } else {
+            return (
+                <div className="flex items-center justify-center">
+                    <BsCheck2Circle className="size-5" color="green" />
+                </div>
+            )
+        }
+    };
+
+    const difficultyBox = (difficulty) => {
+
+        if (difficulty === "Hard") {
+            return (
+                <p className="text-white bg-red-500 rounded-full py-1">{difficulty}</p>
+            )
+        } else if (difficulty === "Moderate") {
+            return (
+                <p className="text-white bg-orange-500 rounded-full py-1">{difficulty}</p>
+            )
+        } else {
+            return (
+                <p className="text-white bg-green-500 rounded-full py-1">{difficulty}</p>
+            )
+        }
+    }
 
     return (
         <tr key={item.id} className="h-10 border-b-2">
-            <td className="font-noto text-center text-sm">
-                {item.status}
-
+            <td className="">
+                    {iconSelection(item.status)}
             </td>
             <td className="font-noto text-center text-sm">
                 {item.title}
@@ -18,8 +49,11 @@ export default function ChallenageItem ({item}) {
                 {item.category}
 
             </td>
-            <td className="font-noto text-center text-sm">
-                {item.difficulty}
+            <td className="font-noto text-center text-sm py-2 flex justify-center">
+                <div className="w-24">
+                    {difficultyBox(item.difficulty)}
+
+                </div>
 
             </td>
             <td className="font-noto text-center text-sm">
