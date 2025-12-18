1457ec696317e9690f33c1ff76c174c4fa2de95d
Kerr Signs <kerrsigns@Design-1.local>
2025-12-18 16:16:26 +0000

    starting a new problem

--- src/pages/Challenages/Components/CategoryList.jsx
+++ src/pages/Challenages/Components/CategoryList.jsx
@@ -5,7 +5,11 @@ import { useState } from "react"
 
 export default function CategoryList () {
     const resetList = useCategorySelection((state) => state.resetList)
+    const list = useCategorySelection((state) => state.unchangedList)
+    const challenageCategories = list.map((item) => item.category)
+    const catlist = new Set(challenageCategories)
 
+    // Setting the active state on the buttons
     const [active, setactive] = useState(false);
 
     const hanldeClick = () => {
@@ -15,7 +19,7 @@ export default function CategoryList () {
     return (
         <div className="flex flex-row gap-3 mb-6">
             <button onClick={() => resetList()} className="h-7 px-3 bg-slate-200 rounded-full hover:bg-cla-primary hover:text-white transition-all hover:-translate-y-1">All</button>
-            {categoriesArray.map((c) => <Category categoryName={c} />)}
+            {challenageCategories.map((c) => <Category categoryName={c} />)}
             
         </div>
     )
